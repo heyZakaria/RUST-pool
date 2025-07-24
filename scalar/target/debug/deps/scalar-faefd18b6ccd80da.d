e060e066402c26005d892189460cccee283030c1
zabdelal <zaki.abdo82@gmail.com>
2025-07-24 10:56:00 +0100

    delete target folder

--- scalar/target/debug/deps/scalar-faefd18b6ccd80da.d
+++
@@ -1,5 +0,0 @@
-/home/zabdelal/Desktop/piscine-rust/scalar/target/debug/deps/scalar-faefd18b6ccd80da.d: src/main.rs
-
-/home/zabdelal/Desktop/piscine-rust/scalar/target/debug/deps/libscalar-faefd18b6ccd80da.rmeta: src/main.rs
-
-src/main.rs:
