3030dc5e5696424535ff27bef2def344c8c07c2f
zabdelal <zaki.abdo82@gmail.com>
2025-07-24 10:40:29 +0100

    sir 3alah

---
+++ scalar/target/debug/deps/scalar-06d8e8fe5ca55dc4.d
@@ -0,0 +1,5 @@
+/home/zabdelal/Desktop/piscine-rust/scalar/target/debug/deps/scalar-06d8e8fe5ca55dc4.d: src/lib.rs
+
+/home/zabdelal/Desktop/piscine-rust/scalar/target/debug/deps/libscalar-06d8e8fe5ca55dc4.rmeta: src/lib.rs
+
+src/lib.rs:
---
+++ scalar/target/debug/deps/scalar-f80e6bc916d06c96.d
@@ -0,0 +1,5 @@
+/home/zabdelal/Desktop/piscine-rust/scalar/target/debug/deps/scalar-f80e6bc916d06c96.d: src/lib.rs
+
+/home/zabdelal/Desktop/piscine-rust/scalar/target/debug/deps/libscalar-f80e6bc916d06c96.rmeta: src/lib.rs
+
+src/lib.rs:
