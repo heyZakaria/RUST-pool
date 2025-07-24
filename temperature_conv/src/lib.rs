de1f1cd640f7915dbfcdb4d15b693847fba5f266
zabdelal <zaki.abdo82@gmail.com>
2025-07-24 12:02:28 +0100

    sir 3alah

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,10 +1,7 @@
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    (f - 32.0) * (5.0 / 9.0) 
-
+    return (f - 32.0) *  1.0 / (9.0 / 5.0);
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
-    let x: f64 = format!("{:.2}", ((c * 9.0/5.0) + 32.0)).parse().unwrap();
-
-    return x
+    (c * 9.0 / 5.0) + 32.0
 }
