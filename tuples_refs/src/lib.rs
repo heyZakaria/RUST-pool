ce7c90935518c86e2cffb1f34f96f4c89aa273eb
zabdelal <zaki.abdo82@gmail.com>
2025-07-25 09:30:00 +0100

    0

---
+++ tuples_refs/src/lib.rs
@@ -0,0 +1,13 @@
+pub struct Student{
+    id:i32
+    first_name:&str
+    last_name:&str
+}
+pub fn id(student: &Student) -> u32 {
+}
+
+pub fn first_name(student: &Student) -> &str {
+}
+
+pub fn last_name(student: &Student) -> &str {
+}
