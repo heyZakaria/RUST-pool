c8efdf441f8cbf1d3b0836708b6b911ff17784b0
zabdelal <zaki.abdo82@gmail.com>
2025-07-27 08:53:16 +0100

    wah

---
+++ borrow_me_the_reference/src/lib.rs
@@ -0,0 +1,10 @@
+pub fn delete_and_backspace(s: &mut String) {
+    for v in s.chars().enumerate(){
+        if v == '+' || v == '-'{
+            v.replace(' ');
+        }
+    }
+}
+
+pub fn do_operations(v: &mut [String]) {
+}
