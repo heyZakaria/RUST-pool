288f2f55c32a82aaafc3d1830ff95ead4f1c8294
zabdelal <zaki.abdo82@gmail.com>
2025-08-02 00:07:46 +0100

    siiiiiiiir

---
+++ unwrap_or_expect/src/lib.rs
@@ -0,0 +1,30 @@
+
+pub enum Security{
+    Unknown,
+    Message, 
+    Warning,
+    NotFound,
+    UnexpectedUrl
+}
+
+pub fn fetch_data(server: Result<&str, &str>, security_level: Security) -> String{
+   
+    match server {
+        Ok(url)=>  {
+            match security_level {
+                Security::UnexpectedUrl => panic!("Not found: {}", url),
+                _=> url.to_string(),
+
+            }
+        },
+        Err(err) => {
+            match security_level {
+                Security::Unknown => panic!("called `Result::unwrap()` on an `Err` value: \"ERROR CRITICAL\""),
+                Security::Message => panic!("ERROR: program stops"),
+                Security::Warning => format!("WARNING: check the server"),
+                Security::NotFound => format!("Not found: {}", err),
+                Security::UnexpectedUrl => panic!("Not found: {}", err),
+            }
+        }
+    }
+}
