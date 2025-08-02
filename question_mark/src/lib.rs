4b1861186cc190a1b5bac85d08b8915ca7104179
zabdelal <zaki.abdo82@gmail.com>
2025-08-02 03:10:22 +0100

    siiiiiiiir

---
+++ question_mark/src/lib.rs
@@ -0,0 +1,19 @@
+pub struct One {
+    first_layer: Option<Two>,
+}
+
+impl One {
+    pub fn get_fourth_layer(self) -> Option<u16> {
+        Some(self.first_layer?.second_layer?.third_layer?.fourth_layer?)
+    }
+}
+
+pub struct Two {
+    second_layer: Option<Three>,
+}
+pub struct Three {
+    third_layer: Option<Four>,
+}
+pub struct Four {
+    fourth_layer: Option<u16>,
+}
