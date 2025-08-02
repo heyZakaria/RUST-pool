a49b67a0434b4869a3e7f31380e553644ffd706c
zabdelal <zaki.abdo82@gmail.com>
2025-08-02 01:27:31 +0100

    siiiiiiiir

--- handling/src/lib.rs
+++ handling/src/lib.rs
@@ -1,19 +1,27 @@
 use std::path::Path;
-use std::io::Read;
-use std::fs::File;
-use std::io::{ ErrorKind };
-use std::fs::OpenOptions;
+use std::fs::{File, OpenOptions};
+use std::io::{ErrorKind };
+// use std::io::Write;
 
 pub fn open_or_create<P: AsRef<Path>>(path: &P, content: &str) {
     match OpenOptions::new().read(true).write(true).append(true).create(true).open(path) {
         Ok(mut file) => {
-            file.write_all(content.as_bytes().expect("Errr"));
+            let x =file.write_all(content.as_bytes());
+            match x {
+                        Ok(()) => () ,
+                        Err(_) => panic!(),
+                    }
         }
         Err(e) =>
             match e.kind() {
                 ErrorKind::NotFound => {
                     let mut f = File::create(path).expect("Errr");
-                    f.write_all(content.as_bytes().expect("Errr"));
+                    let x = f.write_all(content.as_bytes());
+                    match x {
+                        Ok(()) => () ,
+                        Err(_) => panic!(),
+                    }
+                    
                 }
                 _ => panic!(),
             }
